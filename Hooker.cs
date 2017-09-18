dd5def78b23ec7c33c17f2022cbdcc08a7a293c4
viteo <viteo@lab>
2017-09-18 15:05:51 +0300

    Add project files.

---
+++ Hooker.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using Clipboard = System.Windows.Clipboard;
+
+namespace ClipboardRing
+{
+    public class StringWrap
+    {
+        public string Value { get; set; }
+
+        public StringWrap()
+        {
+        }
+
+        public StringWrap(string x)
+        {
+            Value = x;
+        }
+
+        public static implicit operator string(StringWrap x)
+        {
+            return x.Value ?? "";
+        }
+
+        public static implicit operator StringWrap(string x)
+        {
+            return new StringWrap(x);
+        }
+    }
+
+    class Hooker : INotifyPropertyChanged
+    {
+        public bool IsCtrlDown = false;
+        private int _index = 0;
+        private ObservableCollection<StringWrap> _strings = new ObservableCollection<StringWrap>() {""};
+
+        public int index
+        {
+            get { return _index; }
+            set
+            {
+                if (value < 0)
+                    return;
+                _index = value < strings.Count ? value : strings.Count - 1;
+                OnPropertyChanged();
+                Clipboard.SetText(strings[_index]);
+            }
+        }
+        public ObservableCollection<StringWrap> strings
+        {
+            get { return _strings; }
+            set
+            {
+                _strings = value;
+                index = value.Count - 1;
+            }
+        }
+
+
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        public Hooker()
+        {
+            index = 0;
+            Clipboard.SetText(strings[index]);
+        }
+
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            //m_GlobalHook.KeyPress += GlobalHookKeyPress;
+            m_GlobalHook.KeyDown += GlobalHookKeyDown;
+            m_GlobalHook.KeyUp += GlobalHookKeyUp;
+        }
+
+        private void GlobalHookKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.LControlKey)
+                IsCtrlDown = false;
+            if (e.KeyCode == Keys.V && IsCtrlDown)
+            {
+                index = (index + 1) % strings.Count;
+            }
+        }
+
+        private void GlobalHookKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.LControlKey)
+                IsCtrlDown = true;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("KeyPress: \t{0}", e.KeyChar);
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.KeyDown -= GlobalHookKeyDown;
+            m_GlobalHook.KeyUp -= GlobalHookKeyUp;
+            //m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+
+            //It is recommened to dispose it
+            m_GlobalHook.Dispose();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClipboardRing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hooker hooker = new Hooker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Strings.DataContext = hooker;
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            hooker.Subscribe();
+        }
+
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            hooker.Unsubscribe();
+        }
+    }
+}
